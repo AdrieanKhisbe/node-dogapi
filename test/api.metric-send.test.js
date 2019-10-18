8d9b400243202d7c87ddf46781073824a3244aa4
Adriean Khisbe <adriean.khisbe@live.fr>
2019-10-18 18:07:13 +0200

    Replace all ava assertion from true to assert for power assert :memo:

--- test/api.metric-send.test.js
+++ test/api.metric-send.test.js
@@ -19,7 +19,7 @@ test('#send should make a valid api call', t => {
   metric.send('metric.send', [[now, 500]]);
 
   // Assert we properly called `client.request`
-  t.true(spy.calledOnce);
+  t.assert(spy.calledOnce);
   const call_args = spy.getCall(0).args;
   // Method and endpoint are correct
   t.is(call_args[0], 'POST');
@@ -29,28 +29,28 @@ test('#send should make a valid api call', t => {
   // { body: series: [ {metric: "metric.send", host: undefined, tags: undefined, type: undefined} ] }
   // DEV: host/tags/type are optional and should be undefined for this case
   const data = call_args[2];
-  t.true(_.has('body', data));
-  t.true(_.has('series', data.body));
+  t.assert(_.has('body', data));
+  t.assert(_.has('series', data.body));
 
   // Assert we have only 1 series
   // series = [ {metric: "", ...}, ... ]
   const series = data.body.series;
-  t.true(Array.isArray(series));
+  t.assert(Array.isArray(series));
   t.is(series.length, 1);
 
   // Assert the first series is properly formatted
   // first_series = {metric: "", points: [], ...}
   const first_series = series[0];
   t.is(first_series.metric, 'metric.send');
-  t.true(Array.isArray(first_series.points));
+  t.assert(Array.isArray(first_series.points));
   t.deepEqual(first_series.points, [[now, 500]]);
 
   // These keys are optional and should be set, but undefined
-  t.true(_.has('host', first_series));
+  t.assert(_.has('host', first_series));
   t.is(first_series.host, undefined);
-  t.true(_.has('tags', first_series));
+  t.assert(_.has('tags', first_series));
   t.is(first_series.tags, undefined);
-  t.true(_.has('type', first_series));
+  t.assert(_.has('type', first_series));
   t.is(first_series.type, undefined);
 });
 
@@ -60,7 +60,7 @@ test('should properly normalize values to points', t => {
   metric.send('metrics.send.normalize', 500);
 
   // Assert we called `client.request` with the correct `points`
-  t.true(spy.calledOnce);
+  t.assert(spy.calledOnce);
   const call_args = spy.getCall(0).args;
   // { body: series: [ {points: [], }, ] }
   const body = call_args[2].body;
@@ -68,12 +68,12 @@ test('should properly normalize values to points', t => {
 
   // points = [ [<timestamp>, 500] ]
   const points = body.series[0].points;
-  t.true(Array.isArray(points));
+  t.assert(Array.isArray(points));
   t.is(points.length, 1);
 
   // point = [<timestamp>, 500]
   const point = points[0];
-  t.true(Array.isArray(point));
+  t.assert(Array.isArray(point));
   t.is(point.length, 2);
   t.is(point[1], 500);
 });
@@ -84,7 +84,7 @@ test('should properly normalize array of values to points', t => {
   metric.send('metrics.send.normalize', [500, 1000]);
 
   // Assert we called `client.request` with the correct `points`
-  t.true(spy.calledOnce);
+  t.assert(spy.calledOnce);
   const call_args = spy.getCall(0).args;
   // { body: series: [ {points: [], }, ] }
   const body = call_args[2].body;
@@ -92,18 +92,18 @@ test('should properly normalize array of values to points', t => {
 
   // points = [ [<timestamp>, 500], [<timestamp>, 1000] ]
   const points = body.series[0].points;
-  t.true(Array.isArray(points));
+  t.assert(Array.isArray(points));
   t.is(points.length, 2);
 
   // point = [<timestamp>, 500]
   let point = points[0];
-  t.true(Array.isArray(point));
+  t.assert(Array.isArray(point));
   t.is(point.length, 2);
   t.is(point[1], 500);
 
   // point = [<timestamp>, 1000]
   point = points[1];
-  t.true(Array.isArray(point));
+  t.assert(Array.isArray(point));
   t.is(point.length, 2);
   t.is(point[1], 1000);
 });
@@ -115,7 +115,7 @@ test('should not normalize correctly formatted points', t => {
   metric.send('metrics.send.normalize', [[now, 1000]]);
 
   // Assert we called `client.request` with the correct `points`
-  t.true(spy.calledOnce);
+  t.assert(spy.calledOnce);
   const call_args = spy.getCall(0).args;
   // { body: series: [ {points: [], }, ] }
   const body = call_args[2].body;
@@ -123,12 +123,12 @@ test('should not normalize correctly formatted points', t => {
 
   // points = [ [<timestamp>, 1000], ]
   const points = body.series[0].points;
-  t.true(Array.isArray(points));
+  t.assert(Array.isArray(points));
   t.is(points.length, 1);
 
   // point = [<timestamp>, 500]
   const point = points[0];
-  t.true(Array.isArray(point));
+  t.assert(Array.isArray(point));
   t.deepEqual(point, [now, 1000]);
 });
 
@@ -138,7 +138,7 @@ test('should properly set metric type', t => {
   metric.send('metrics.send.counter', 5, {type: 'count'});
 
   // Assert we called `client.request` with the correct `points`
-  t.true(spy.calledOnce);
+  t.assert(spy.calledOnce);
   const call_args = spy.getCall(0).args;
   // { body: series: [ {points: [], }, ] }
   const body = call_args[2].body;
@@ -147,14 +147,14 @@ test('should properly set metric type', t => {
   // Assert we have only 1 series
   // series = [ {metric: "", ...}, ... ]
   const series = body.series;
-  t.true(Array.isArray(series));
+  t.assert(Array.isArray(series));
   t.is(series.length, 1);
 
   // Assert the first series is properly formatted
   // first_series = {metric: "", type: "count", points: [], ...}
   const first_series = series[0];
   t.is(first_series.metric, 'metrics.send.counter');
-  t.true(_.has('type', first_series));
+  t.assert(_.has('type', first_series));
   t.is(first_series.type, 'count');
 });
 
@@ -164,7 +164,7 @@ test('should properly set convert metric_type to type', t => {
   metric.send('metrics.send.counter', 5, {metric_type: 'count'});
 
   // Assert we called `client.request` with the correct `points`
-  t.true(spy.calledOnce);
+  t.assert(spy.calledOnce);
   const call_args = spy.getCall(0).args;
   // { body: series: [ {points: [], }, ] }
   const body = call_args[2].body;
@@ -173,13 +173,13 @@ test('should properly set convert metric_type to type', t => {
   // Assert we have only 1 series
   // series = [ {metric: "", ...}, ... ]
   const series = body.series;
-  t.true(Array.isArray(series));
+  t.assert(Array.isArray(series));
   t.is(series.length, 1);
 
   // Assert the first series is properly formatted
   // first_series = {metric: "", type: "count", points: [], ...}
   const first_series = series[0];
   t.is(first_series.metric, 'metrics.send.counter');
-  t.true(_.has('type', first_series));
+  t.assert(_.has('type', first_series));
   t.is(first_series.type, 'count');
 });
